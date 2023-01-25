0ba9cc3400e3d6b7e6e5cffa6d75bd191698d493
Aniket Kumar <aniketkumar693@gmail.com>
2023-01-24 19:27:29 -0600

    Create two-sum.py

---
+++ grind-169/two-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        map_nums = {}
+        for i in range(len(nums)):
+            # complement is the difference between target and the current number
+            complement = target - nums[i]
+            # checking if complement is in map_nums and if it is not the same index
+            if complement in map_nums and map_nums[complement] != i:
+                return [i, map_nums[complement]]
+            map_nums[nums[i]] = i
+
+nums = [3,2,4]
+target = 6
+soln = Solution().twoSum(nums, target)
+print(soln)
