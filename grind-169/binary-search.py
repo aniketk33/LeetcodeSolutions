9c9645612a22b4c5b9b5e21b2a1a8b0b361df3fb
Aniket Kumar <aniketkumar693@gmail.com>
2023-04-10 17:28:52 -0500

    binary search implemented

---
+++ grind-169/binary-search.py
@@ -0,0 +1,17 @@
+def search(nums, target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        if target > nums[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 2
+result = search(nums, target)
+print(result)
