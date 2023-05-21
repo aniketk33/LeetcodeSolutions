b559b86bc5f10fbba3b033f7f13039c514fb7041
Aniket Kumar <aniketkumar693@gmail.com>
2023-05-21 16:30:24 -0700

    minor fixes

--- grind-169/valid-parentheses.py
+++ grind-169/valid-parentheses.py
@@ -1,28 +1,29 @@
-'''Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+"""Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 An input string is valid if:
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
-Every close bracket has a corresponding open bracket of the same type.'''
+Every close bracket has a corresponding open bracket of the same type."""
 
-class Solution:
-    def is_valid(self, s: str) -> bool:
-        stack = []
-        for char in s:
-            if char == '(' or char == '{' or char == '[':
-                stack.append(char)
+
+def is_valid(s: str) -> bool:
+    stack = []
+    for char in s:
+        if char == '(' or char == '{' or char == '[':
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            if char == ')' and stack[-1] == '(':
+                stack.pop()
+            elif char == '}' and stack[-1] == '{':
+                stack.pop()
+            elif char == ']' and stack[-1] == '[':
+                stack.pop()
             else:
-                if not stack:
-                    return False
-                if char == ')' and stack[-1] == '(':
-                    stack.pop()
-                elif char == '}' and stack[-1] == '{':
-                    stack.pop()
-                elif char == ']' and stack[-1] == '[':
-                    stack.pop()
-                else:
-                    return False
-        return not stack
+                return False
+    return not stack
 
-s = "(){[[((((((()))))))]]}[]"
-soln = Solution().is_valid(s)
-print(soln)
+
+input_str = "(){[[((((((()))))))]]}[]"
+result = is_valid(input_str)
+print(result)
