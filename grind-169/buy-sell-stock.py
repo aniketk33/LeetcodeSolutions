fc0a3383f8189e43d5e0503f1743d771ba05aad5
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-23 00:46:09 -0400

    code formatted

--- grind-169/buy-sell-stock.py
+++ grind-169/buy-sell-stock.py
@@ -6,7 +6,7 @@ def max_stock_profit(price_arr):
     buy_ptr = 0
     sell_ptr = 1
     while sell_ptr < arr_len:
-        curr_profit = price_arr[sell_ptr]-price_arr[buy_ptr]
+        curr_profit = price_arr[sell_ptr] - price_arr[buy_ptr]
         if price_arr[buy_ptr] < price_arr[sell_ptr]:
             max_profit = max(max_profit, curr_profit)
         else:
@@ -16,6 +16,6 @@ def max_stock_profit(price_arr):
     return max_profit
 
 
-prices = [7,1,5,3,6,4]
+prices = [7, 1, 5, 3, 6, 4]
 res = max_stock_profit(prices)
 print(res)
