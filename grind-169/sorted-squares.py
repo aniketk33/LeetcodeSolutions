55ee15efaa51cfb6e2c24c4416c0dd95be0ca916
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-25 23:26:20 -0400

    solution for 977

---
+++ grind-169/sorted-squares.py
@@ -0,0 +1,28 @@
+"""Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+"""
+
+
+def sorted_squares(num_arr):
+    sorted()
+    arr_len = len(num_arr)
+    if arr_len == 1:
+        return [num_arr[0] ** 2]
+    left_ptr, right_ptr = 0, arr_len - 1
+    result = []
+    while right_ptr >= left_ptr:
+        if num_arr[left_ptr] ** 2 > num_arr[right_ptr] ** 2:
+            result.insert(0, num_arr[left_ptr] ** 2)
+            left_ptr += 1
+        else:
+            result.insert(0, num_arr[right_ptr] ** 2)
+            right_ptr -= 1
+
+    return result
+
+
+nums = [-7, -3, 2, 3, 11]
+res = sorted_squares(nums)
+print(res)
