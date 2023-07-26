a2e6a7401bfe221e9dcaa82177f70b8a779623a8
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-25 23:27:34 -0400

    minor fixes

--- grind-169/sorted-squares.py
+++ grind-169/sorted-squares.py
@@ -1,6 +1,8 @@
-"""Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+"""Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted
+in non-decreasing order.
 
-Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different
+approach?
 
 """
 
