19262322f09113f7427d34de4831d495bdade6ac
Aniket Kumar <aniketkumar693@gmail.com>
2023-10-02 22:49:45 -0500

    solution for 49

---
+++ group-anagrams.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+
+def group_anagrams(words):
+    # hashmap to store the count of anagrams
+    char_count_dict = defaultdict(list)
+
+    for word in words:
+        # create a count array to store alphabets for each word
+        count = [0] * 26
+        for c in word:
+            arr_location = ord(c) - ord('a')
+            count[arr_location] += 1
+        # cannot create list as keys in python op change to tuple
+        char_count_dict[tuple(count)].append(word)
+
+    return char_count_dict.values()
+
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+res = group_anagrams(strs)
+print(res)
