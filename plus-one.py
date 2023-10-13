d85e56ac45dcf577331c1f73f570d21501b8ac1e
Aniket Kumar <aniketkumar693@gmail.com>
2023-10-13 00:07:01 -0500

    modified solution for 66

--- plus-one.py
+++ plus-one.py
@@ -1,25 +1,32 @@
-'''66. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's. Increment the large integer by one and return the resulting array of digits.
+"""66. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's. Increment the large integer by one and return the resulting array of digits.
 Input: digits = [1,2,3]
 Output: [1,2,4]
 Explanation: The array represents the integer 123.
 Incrementing by one gives 123 + 1 = 124.
 Thus, the result should be [1,2,4].
-'''
-
-class Solution:
-    def plusOne(self, digits: list):
-        number_str = ''
-        for digit in digits:
-            number_str += str(digit)
-        
-        number_int = int(number_str)
-        number_int += 1
-        number_str = str(number_int)
-        digit_list = list(number_str)
-
-        return digit_list
-        
+"""
 
 
-digits = [9]
-op = Solution().plusOne(digits)
+def plus_one(digits_arr):
+    arr_len = len(digits_arr)
+    carry = 0
+    # last digit operation
+    last_digit = digits_arr[-1]
+    last_digit = carry + last_digit + 1
+    carry = last_digit // 10
+    last_digit %= 10
+    digits_arr[-1] = last_digit
+    if carry:
+        for i in range(arr_len - 2, -1, -1):
+            last_digit = digits_arr[i]
+            last_digit = carry + last_digit
+            carry = last_digit // 10
+            last_digit %= 10
+            digits_arr[i] = last_digit
+        if carry:
+            digits_arr.insert(0, carry)
+
+
+arr = [9, 9, 9]
+plus_one(arr)
+print(arr)
