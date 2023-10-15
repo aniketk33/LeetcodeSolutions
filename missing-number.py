dc6af3cb9b6f6ac24c8d90dff18a8c014fc02cff
Aniket Kumar <aniketkumar693@gmail.com>
2023-10-15 17:20:12 -0500

    minor fixes

--- missing-number.py
+++ missing-number.py
@@ -1,15 +1,17 @@
-'''Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
-'''
+"""Given an array nums containing n distinct numbers in the range [0, n],
+return the only number in the range that is missing from the array.
+"""
 
-class Solution:
-    def missingNumber(self, nums: list) -> int:
-        # counting the length
-        N = len(nums)
-        # get the total sum of N elements
-        sum_of_all = N*(N+1)//2
-        # by subtracting the sum of given arr and total sum, we get the missing number
-        return sum_of_all - sum(nums)
 
-nums = [3,1,0]
-sol = Solution().missingNumber(nums)
+def missing_number(num_arr: list) -> int:
+    # counting the length
+    arr_len = len(num_arr)
+    # get the total sum of N elements using (N*(N+1))//2 formula
+    sum_of_all = arr_len * (arr_len + 1) // 2
+    # by subtracting the sum of given arr and total sum, we get the missing number
+    return sum_of_all - sum(num_arr)
+
+
+nums = [3, 1, 0]
+sol = missing_number(nums)
 print(sol)
