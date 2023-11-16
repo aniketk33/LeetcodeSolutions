1aa7d58fa247ab1b8cf22357f040312792a2d060
Aniket Kumar <aniketkumar693@gmail.com>
2023-11-15 22:26:44 -0600

    solution for 680

---
+++ two-pointers/valid-palindrome-2.py
@@ -0,0 +1,19 @@
+# return True or False if the string is palindrome or not if we remove a char
+def is_palindrome(s):
+    left_ptr, right_ptr = 0, len(s) - 1
+
+    while left_ptr < right_ptr:
+        if s[left_ptr] != s[right_ptr]:
+            # skip left and right char. why +1? because it is non-inclusive
+            skip_left, skip_right = s[left_ptr + 1: right_ptr + 1], s[left_ptr:right_ptr]
+            # reverse the removed character string
+            return skip_left == skip_left[::-1] or skip_right == skip_right[::-1]
+
+        left_ptr += 1
+        right_ptr -= 1
+
+    return True
+
+
+res = is_palindrome('abca')
+print(res)
