87c6d9b0412c24a24df95bc79f4f7dc2763e8c0b
Aniket Kumar <aniketkumar693@gmail.com>
2023-12-21 17:16:45 -0800

    solution for 1822

---
+++ arrays/sign-of-the-product.py
@@ -0,0 +1,23 @@
+def sign(nums):
+    # if 0 in nums:
+    #     return 0
+    #
+    # product = 1
+    # for num in nums:
+    #     product *= num
+    #
+    # return 1 if product > 0 else -1
+    # NOTE: Calculating the product is not necessary (Read the problem description).
+    # Only calculating the negative numbers will save integer over-overflowing.
+    if 0 in nums:
+        return 0
+    negative_sign = 0
+    for num in nums:
+        if num > 0:
+            continue
+        negative_sign += 1
+    return 1 if negative_sign % 2 == 0 else -1
+
+
+res = sign([-1, -2, -3, -4, 3, 2, 1])
+print(res)
