b5e49116c85b8aaa2dcdc74a3aaceadf943b5880
Aniket Kumar <aniketkumar693@gmail.com>
2024-04-19 13:17:40 -0500

    alternate solution for 347

--- arrays/top_frequent.py
+++ arrays/top_frequent.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 def top_k_frequent(num_arr, k):
     result = []
     # create dict to store frequency
@@ -18,7 +21,28 @@ def top_k_frequent(num_arr, k):
                 return result
 
 
+def top_k_frequent_2(nums, k):
+    """using heaps"""
+    count = {}
+    for num in nums:
+        count[num] = 1 + count.get(num, 0)
+
+    # store the negative count for each num
+    max_heap = []
+    for key, val in count.items():
+        max_heap.append((-val, key))
+
+    heapq.heapify(max_heap)
+    result = []
+    while k > 0:
+        # store the key in the result array
+        result.append(heapq.heappop(max_heap)[1])
+        k -= 1
+
+    return result
+
+
 arr = [1, 1, 1, 2, 2, 3]
 k = 2
-res = top_k_frequent(arr, k)
+res = top_k_frequent_2(arr, k)
 print(res)
