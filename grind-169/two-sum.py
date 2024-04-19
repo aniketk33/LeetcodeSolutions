f55b72997dd3b3af40a52599cdc58f9ed26f1212
Aniket Kumar <aniketkumar693@gmail.com>
2024-04-19 12:32:46 -0500

    solution for 1

--- grind-169/two-sum.py
+++ grind-169/two-sum.py
@@ -1,20 +1,15 @@
-'''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.
-'''
+def two_sum(nums, target):
+    compliments = {}
 
-class Solution:
-    def two_sum(self, nums: list[int], target: int) -> list[int]:
-        map_nums = {}
-        for i in range(len(nums)):
-            # complement is the difference between target and the current number
-            complement = target - nums[i]
-            # checking if complement is in map_nums and if it is not the same index
-            if complement in map_nums and map_nums[complement] != i:
-                return [i, map_nums[complement]]
-            map_nums[nums[i]] = i
+    for i in range(len(nums)):
+        compliment = target - nums[i]
+        # check if the compliment is present in the dict, and it is a different index
+        if compliment in compliments and compliments[compliment] != i:
+            return [i, compliments[compliment]]
 
-nums = [3,2,4]
-target = 6
-soln = Solution().two_sum(nums, target)
-print(soln)
+        # store the index of the current number
+        compliments[nums[i]] = i
+
+
+res = two_sum([3, 2, 4], 6)
+print(res)
