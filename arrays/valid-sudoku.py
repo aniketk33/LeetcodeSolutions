0b3d48274355d1f378ef37b47fad0f9557112f50
Aniket Kumar <aniketkumar693@gmail.com>
2024-04-20 09:48:17 -0500

    minor fixes for 36

--- arrays/valid-sudoku.py
+++ arrays/valid-sudoku.py
@@ -6,23 +6,32 @@ from collections import defaultdict
 def valid_sudoku(board: List[List[str]]) -> bool:
     rows = defaultdict(set)
     columns = defaultdict(set)
-    grid = defaultdict(set)
+    grids = defaultdict(set)
 
     for curr_row in range(9):
         for curr_column in range(9):
             if board[curr_row][curr_column] == '.':
                 continue
 
+            curr_val = board[curr_row][curr_column]
             # check for three conditions.
-            # 1. if it is present in the entire row or column or the current grid
-            if (board[curr_row][curr_column] in rows[curr_row] or
-                    board[curr_row][curr_column] in columns[curr_column] or
-                    board[curr_row][curr_column] in grid[(curr_row // 3, curr_column // 3)]):
+            # 1. if it is present in the entire row or column or the current grids
+            if (curr_val in rows[curr_row] or curr_val in columns[curr_column] or
+                    curr_val in grids[(curr_row // 3, curr_column // 3)]):
                 return False
 
-            # add the curr number in row, column and the grid
+            # add the curr number in row, column and the grids
             rows[curr_row].add(board[curr_row][curr_column])
             columns[curr_column].add(board[curr_row][curr_column])
-            grid[(curr_row // 3, curr_column // 3)].add(board[curr_row][curr_column])
+            grids[(curr_row // 3, curr_column // 3)].add(board[curr_row][curr_column])
 
     return True
+
+
+input_board = [[".", "8", ".", ".", ".", ".", ".", ".", "."], [".", ".", ".", ".", "2", ".", ".", ".", "."],
+               [".", "6", ".", ".", ".", ".", "1", ".", "4"], [".", ".", ".", "9", ".", ".", "7", ".", "."],
+               [".", ".", ".", ".", ".", ".", ".", "4", "."], [".", ".", "1", ".", ".", "8", ".", ".", "."],
+               [".", ".", ".", ".", ".", ".", ".", ".", "."], [".", ".", ".", ".", ".", "5", ".", "7", "."],
+               [".", ".", "3", ".", ".", "5", "6", ".", "."]]
+res = valid_sudoku(input_board)
+print(res)
