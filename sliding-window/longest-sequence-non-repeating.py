0061e1f2ca696bdc939d379bed198bce0e147388
Aniket Kumar <aniketkumar693@gmail.com>
2024-08-21 14:31:28 -0700

    solution fixes for 3

--- sliding-window/longest-sequence-non-repeating.py
+++ sliding-window/longest-sequence-non-repeating.py
@@ -30,6 +30,28 @@ def longest_sequence_2(s):
     return longest
 
 
-res = longest_sequence_2('abccabcabcc')
+# optimal solution
+def longest_sequence_3(s):
+    arr_len = len(s)
+    left, right = 0, 0
+    result = 0
+    # takes O(1) for lookup/delete/insert
+    curr_window = set()
+
+    while right < arr_len:
+        if s[right] in curr_window:
+            while curr_window and s[left] != s[right]:
+                curr_window.remove(s[left])
+                left += 1
+            left += 1
+        result = max(result, right - left + 1)
+        curr_window.add(s[right])
+        right += 1
+
+    return result
+
+
+# res = longest_sequence_2('abccabcabcc')
+res = longest_sequence_3('abccabcabcc')
 # res = longest_sequence('aaaabaaa')
 print(res)
