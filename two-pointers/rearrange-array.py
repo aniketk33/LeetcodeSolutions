bb1e448c1be1e8e7493c937ffbfdf0b2515736a5
Aniket Kumar <aniketkumar693@gmail.com>
2024-08-28 00:20:12 -0700

    solution for 1968

--- two-pointers/rearrange-array.py
+++ two-pointers/rearrange-array.py
@@ -17,5 +17,20 @@ def rearrange(nums):
     return result
 
 
-res = rearrange([6, 2, 0, 9, 7])
+# optimal approach without sorting array and constant space
+def rearrange_2(nums):
+    for idx in range(1, len(nums) - 1):
+        prev = nums[idx - 1]
+        curr = nums[idx]
+        next = nums[idx + 1]
+
+        # swap the current with next only when this condition is satisfied
+        if prev < curr < next or prev > curr > next:
+            nums[idx], nums[idx + 1] = nums[idx + 1], nums[idx]
+
+    return nums
+
+
+# res = rearrange([6, 2, 0, 9, 7])
+res = rearrange_2([6, 2, 0, 9, 7])
 print(res)
