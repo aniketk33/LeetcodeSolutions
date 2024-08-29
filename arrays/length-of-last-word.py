be2b79bb3ae3a93f315e0d4f40f81cf9f5294a6f
Aniket Kumar <aniketkumar693@gmail.com>
2024-08-29 01:07:13 -0700

    solution for 58

--- arrays/length-of-last-word.py
+++ arrays/length-of-last-word.py
@@ -5,5 +5,20 @@ def last_word(s: str):
     return len(word)
 
 
-res = last_word('   fly me   to   the moon  ')
+def last_word_2(s):
+    count, last_idx = 0, len(s) - 1
+    # skip all the white spaces from the end
+    while last_idx >= 0 and s[last_idx] == ' ':
+        last_idx -= 1
+
+    # get to the first char index for the given word
+    while last_idx >= 0 and s[last_idx] != ' ':
+        count += 1
+        last_idx -= 1
+
+    return count
+
+
+# res = last_word('   fly me   to   the moon  ')
+res = last_word_2('   fly me   to   the moon  ')
 print(res)
