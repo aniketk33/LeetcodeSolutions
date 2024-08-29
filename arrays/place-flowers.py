675cfc8b58e769c510f48dd73dc280fa46638690
Aniket Kumar <aniketkumar693@gmail.com>
2024-08-29 14:52:27 -0700

    solution for 605

--- arrays/place-flowers.py
+++ arrays/place-flowers.py
@@ -12,6 +12,24 @@ def place_flowers(flower_bed, n):
     return n <= 0
 
 
-flowerbed = [1, 0, 0, 0, 0, 0, 1]
-res = place_flowers(flowerbed, 2)
+def place_flowers_2(flower_bed, n):
+    idx = 0
+
+    while idx < len(flower_bed):
+        # check for three things
+        # 1. if it is a 0
+        # 2. if the prev value is 0
+        # 3. the next value should be 0 too
+        if (flower_bed[idx] == 0 and (idx == 0 or flower_bed[idx - 1] == 0) and
+                (idx == len(flower_bed) - 1 or flower_bed[idx + 1] == 0)):
+            flower_bed[idx] = 1
+            n -= 1
+        idx += 1
+
+    return n <= 0
+
+
+flowerbed = [1,0,0,0,0,1]
+# res = place_flowers(flowerbed, 2)
+res = place_flowers_2(flowerbed, 2)
 print(res)
