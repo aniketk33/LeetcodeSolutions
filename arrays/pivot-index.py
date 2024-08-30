a07a72d70f3c6e20583fc6144d36a9cfeb80f356
Aniket Kumar <aniketkumar693@gmail.com>
2024-08-30 01:05:39 -0700

    solution for 724

--- arrays/pivot-index.py
+++ arrays/pivot-index.py
@@ -28,6 +28,42 @@ def pivot_index_2(nums):
     return -1
 
 
+def pivot_index_3(nums):
+    prefix_arr = [0] * len(nums)
+    postfix_arr = [0] * len(nums)
+    prefix_sum = postfix_sum = 0
+
+    # creating a prefix array
+    for i in range(len(nums)):
+        prefix_arr[i] += prefix_sum
+        prefix_sum += nums[i]
+
+    # creating a postfix array
+    for i in range(len(nums) - 1, -1, -1):
+        postfix_arr[i] += postfix_sum
+        postfix_sum += nums[i]
+
+    for i in range(len(nums)):
+        if prefix_arr[i] == postfix_arr[i]:
+            return i
+
+    return -1
+
+
+def pivot_index_4(nums):
+    left, right = 0, sum(nums)
+
+    for i, v in enumerate(nums):
+        right -= v
+        if left == right:
+            return i
+        left += v
+
+    return -1
+
+
 input_nums = [1, 7, 3, 6, 5, 6]
-res = pivot_index_2(input_nums)
+# res = pivot_index_2(input_nums)
+# res = pivot_index_3(input_nums)
+res = pivot_index_4(input_nums)
 print(res)
