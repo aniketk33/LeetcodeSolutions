c444a783d9de013aa63f1f0d3c7d0e8219e7629d
Aniket Kumar <aniketkumar693@gmail.com>
2024-09-08 11:27:26 -0700

    minor fixes for 424

--- sliding-window/longest-repeating-char-replace.py
+++ sliding-window/longest-repeating-char-replace.py
@@ -7,15 +7,26 @@ def replace_char(s, k):
     while right_ptr < len(s):
         curr_char = s[right_ptr]
         char_count_dict[curr_char] = 1 + char_count_dict.get(curr_char, 0)
+
         # get the max frequency compared with the recent updated char count
-        max_frequency = max(max_frequency, char_count_dict[curr_char])
+        # max_frequency = max(max_frequency, char_count_dict[curr_char])
+
+        # this improves runtime
+        if char_count_dict[curr_char] > max_frequency:
+            max_frequency = char_count_dict[curr_char]
+
         # if diff is greater than the threshold, then keep moving the left pointer and update the count
         # get the difference using window length - frequent char count
         while (right_ptr - left_ptr + 1) - max_frequency > k:
             char_count_dict[s[left_ptr]] -= 1
             left_ptr += 1
+
         # update the result which is the window_len
-        result = max(result, right_ptr - left_ptr + 1)
+        # result = max(result, right_ptr - left_ptr + 1)
+
+        # this improves runtime
+        if right_ptr - left_ptr + 1 > result:
+            result = right_ptr - left_ptr + 1
 
         right_ptr += 1
 
