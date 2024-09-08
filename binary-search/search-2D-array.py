b76d6833b3aad1e36a0135b9fc472502d911da33
Aniket Kumar <aniketkumar693@gmail.com>
2024-09-08 00:30:01 -0700

    solution for 74

--- binary-search/search-2D-array.py
+++ binary-search/search-2D-array.py
@@ -8,17 +8,53 @@ You must write a solution in O(log(m * n)) time complexity."""
 
 
 def search_matrix(num_arr, target):
-    i, j = 0, len(num_arr[0]) - 1
-    while 0 <= i < len(num_arr) and 0 <= j < len(num_arr[0]):
-        if num_arr[i][j] == target:
+    row, col = 0, len(num_arr[0]) - 1
+    while 0 <= row < len(num_arr) and 0 <= col < len(num_arr[0]):
+        if num_arr[row][col] == target:
             return True
-        if num_arr[i][j] > target:
-            j -= 1
+        if num_arr[row][col] > target:
+            col -= 1
         else:
-            i += 1
+            row += 1
 
     return False
 
 
-res = search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)
+def search_matrix_2(matrix, target):
+    # define row and column variables
+    row_top, row_bot = 0, len(matrix) - 1
+    col_left, col_right = 0, len(matrix[0]) - 1
+    mid_row = 0
+
+    # get the mid-row index
+    while row_top <= row_bot:
+        mid_row = row_top + (row_bot - row_top) // 2
+        if matrix[mid_row][0] == target or matrix[mid_row][-1] == target:
+            return True
+        # if the target is present above the mid-row
+        if matrix[mid_row][0] > target:
+            row_bot = mid_row - 1
+        # if below the mid-row
+        elif matrix[mid_row][-1] < target:
+            row_top = mid_row + 1
+        # if it is in the mid-row:
+        else:
+            break
+
+    # get the mid-column
+    while col_left <= col_right:
+        mid_col = col_left + (col_right - col_left) // 2
+
+        if matrix[mid_row][mid_col] == target:
+            return True
+        if matrix[mid_row][mid_col] > target:
+            col_right = mid_col - 1
+        else:
+            col_left = mid_col + 1
+
+    return False
+
+
+# res = search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)
+res = search_matrix_2([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)
 print(res)
