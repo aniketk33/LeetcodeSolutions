0bc05bbe0fb4297c36c4246043fccf1aaf7e74cb
Aniket Kumar <aniketkumar693@gmail.com>
2024-09-08 22:04:56 -0700

    solution for 33

--- binary-search/search-in-rotated-sorted-array.py
+++ binary-search/search-in-rotated-sorted-array.py
@@ -41,3 +41,29 @@ def search(num_arr, target) -> int:
         target_index = binary_search(num_arr, left=min_index, right=n - 1, target=target)
 
     return target_index
+
+
+def search_2(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        # determine which side is sorted
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+    return -1
+
+
+res = search_2([4, 5, 6, 7, 0, 1, 2], 0)
+print(res)
