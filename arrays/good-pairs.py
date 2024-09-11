e320b3a08873cc03edc97901f5fc0734a047b3fa
Aniket Kumar <aniketkumar693@gmail.com>
2024-09-10 17:39:28 -0700

    solution for 1512

--- arrays/good-pairs.py
+++ arrays/good-pairs.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def good_pairs(nums):
     result = 0
     count_pairs = {}
@@ -15,5 +18,28 @@ def good_pairs(nums):
     return result
 
 
-res = good_pairs([1, 2, 3, 1, 1, 3])
+# optimal solution
+def good_pairs_2(nums):
+    count = Counter(nums)
+    pairs = 0
+
+    for key, val in count.items():
+        if val <= 1:
+            continue
+        pairs += (val * (val - 1)) // 2
+
+    return pairs
+    # OR
+    # count = defaultdict(int)
+    # pairs = 0
+    #
+    # for num in nums:
+    #     count[num] += 1
+    #     pairs += count[num] - 1
+    #
+    # return pairs
+
+
+# res = good_pairs([1, 2, 3, 1, 1, 3])
+res = good_pairs_2([1, 2, 3, 1, 1, 3])
 print(res)
