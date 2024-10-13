ca6d1d9d344ce6347c57ae2bd69fa9aeb6e1d707
Aniket Kumar <aniketkumar693@gmail.com>
2024-10-12 21:30:11 -0700

    solution for 118

--- arrays/pascals-triangle.py
+++ arrays/pascals-triangle.py
@@ -15,5 +15,20 @@ def pascals_triangle(n):
     return result
 
 
-res = pascals_triangle(5)
+def pascals_triangle_2(n):
+    triangle = []
+
+    for row in range(n):
+        curr_row = [1] * (row + 1)
+        # we need to iterate from the 1st column
+        for col in range(1, row):
+            # current column's value is [prev_row][curr_col-1] + [prev_row][curr_col]
+            curr_row[col] = triangle[row - 1][col - 1] + triangle[row - 1][col]
+        triangle.append(curr_row)
+
+    return triangle
+
+
+# res = pascals_triangle(5)
+res = pascals_triangle_2(5)
 print(res)
