0bd9d1bd0dda5c159818d170ede65d3468e59933
Aniket Kumar <aniketkumar693@gmail.com>
2026-01-01 19:58:08 +0530

    soln optimized for 125

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -21,6 +21,19 @@ def valid_palindrome(sentence: str):
     return True
 
 
+def valid_palindrome_2(sentence: str):
+    updated_sentence = [c.lower() for c in sentence if c.isalnum()]
+    left_ptr, right_ptr = 0, len(updated_sentence) - 1
+    while left_ptr <= right_ptr:
+        if updated_sentence[left_ptr] != updated_sentence[right_ptr]:
+            return False
+        left_ptr += 1
+        right_ptr -= 1
+
+    return True
+
+
 s = "A man, a plan, a canal: Panama"
-res = valid_palindrome(s)
+# res = valid_palindrome(s)
+res = valid_palindrome_2(s)
 print(res)
